fe5ab06ae862694f7adc72eb68735568e536edca
arimafp <arimafp@mail.ru>
2022-12-16 21:08:11 +0300

    70% of work

---
+++ js/baza.js
@@ -0,0 +1,25 @@
+const suprastin = {
+    name: "Супрастин",
+    type: "лекарство",
+    disease: "аллергия",
+    group: "аптечкаВотпуск",
+    mnn: "хлоропирамин",
+    count: "ТАБ. 25МГ №20",
+    description: "Противоаллергенные препараты",
+    img: "/img/shop_images/13894_1527245715.5746.jpg"
+};
+
+
+
+
+
+
+
+
+
+// Заполнение странички
+
+
+// function fillTemplate(let name) {
+
+// }
